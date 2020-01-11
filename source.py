38f7dd4418e18559d78e062c0c1e00ed49323f9e
Marcin Graja <36998161+trahatar@users.noreply.github.com>
2020-01-11 14:59:04 +0100

    reporting cpu and memory usage

--- source.py
+++ source.py
@@ -1,34 +1,42 @@
 import ctypes
 import time
 import psutil
-keyboardClicks = 0
-mouseClicks = 0
-
-
-def countClicks(countingTime):
+def countClicks(countingTime, updatePeriod):
     start = time.time()
     print("start")
     buttonState = []
-    global mouseClicks
-    global keyboardClicks
+    mouseClicks = 0
+    keyboardClicks = 0
     numberOfKeys = 256
     for i in range(numberOfKeys):
         buttonState.append(0)
-    maximum = 0
+    lastUpdate = time.time()
+    numberOfUpdates = 0
+    cpuUsage = 0
+    memoryUsage = 0
+    psutil.cpu_percent()
     while 1:
+        if (time.time() - lastUpdate) > updatePeriod:
+            cpuUsage += psutil.cpu_percent()
+            memoryUsage += psutil.virtual_memory().percent
+            lastUpdate = time.time()
+            numberOfUpdates += 1
         if (time.time() - start) > countingTime:
             start = time.time()
+            cpuUsage /= numberOfUpdates
+            memoryUsage /= numberOfUpdates
             print("mouse clicks: " + str(mouseClicks))
             print("keyboard clicks: " + str(keyboardClicks))
-            print("max: " + str(maximum))
+            print("cpu usage: " + str(cpuUsage))
+            print("memory usage: " + str(memoryUsage))
+            # call sending
             mouseClicks = 0
             keyboardClicks = 0
-            # call sending
+            cpuUsage = 0
+            memoryUsage = 0
         for x in range(numberOfKeys):
             if ctypes.windll.user32.GetKeyState(x) not in [0, 1]:
                 if buttonState[x] != 1:
-                    if x > maximum:
-                        maximum = x
                     if x < 5:
                         mouseClicks += 1
                         print(str(x) + " pressed (mouse)")
@@ -42,7 +50,11 @@ def countClicks(countingTime):
                 if buttonState[x] != 0:
                     print(str(x) + " released")
                     buttonState[x] = 0
-        time.sleep(0.001)
+        time.sleep(0.01)
 
 
-countClicks(60)
+countClicks(60, 1)
+print("\n\n\n")
+print(psutil.virtual_memory().percent)
+print("\n\n\n")
+print(psutil.cpu_percent(interval=1))
